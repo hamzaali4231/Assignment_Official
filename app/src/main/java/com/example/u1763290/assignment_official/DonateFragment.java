c1c917de6b80e0b4cdb08b6ff80da8c3dda83675
u1763290 <u1763290@unimail.hud.ac.uk>
2019-04-12 13:26:01 +0100

    first commit

---
+++ app/src/main/java/com/example/u1763290/assignment_official/DonateFragment.java
@@ -0,0 +1,21 @@
+package com.example.u1763290.assignment_official;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by u1763290 on 12/04/2019.
+ */
+
+public class DonateFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_donate,container,false);
+    }
+}
--- app/src/main/java/com/example/u1763290/assignment_official/Football_activity.java
+++ app/src/main/java/com/example/u1763290/assignment_official/Football_activity.java
@@ -1,13 +1,75 @@
 package com.example.u1763290.assignment_official;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Football_activity extends AppCompatActivity {
+public class Football_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_footbal_activity);
+        setContentView(R.layout.activity_navigation);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new RulesFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_send);
+        }
     }
-}
+
+    @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem item){
+            switch (item.getItemId()) {
+                case R.id.nav_maps:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new MapsFragment()).commit();
+                    break;
+                case R.id.nav_rules:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new RulesFragment()).commit();
+                    break;
+                case R.id.nav_donate:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new DonateFragment()).commit();
+                    break;
+                case R.id.nav_send:
+                    Toast.makeText(this, "Sent", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }}
+
+    }
+
+
